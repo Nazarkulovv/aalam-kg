43d768aff491ebce549b96a35ce83bb8764c564a
nazarkulovv <ramazanwelll@gmail.com>
2024-07-21 17:09:58 +0600

    new components section

--- src/Pages/HeaderInSectionColors.tsx
+++ src/Pages/HeaderInSectionColors.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header/Header'
+import WholesaleClothing from '@/components/WholesaleClothing/WholesaleClothing'
 
 export default function HeaderInSectionColors() {
 	return (
 		<div className='mainBac'>
 			<Header />
+			<WholesaleClothing />
 		</div>
 	)
 }
--- src/components/WholesaleClothing/WholesaleClothing.tsx
+++ src/components/WholesaleClothing/WholesaleClothing.tsx
@@ -1,3 +1,90 @@
+'use client'
+import { useState } from 'react'
+import { BiPhoneCall } from 'react-icons/bi'
+import { FaTelegramPlane } from 'react-icons/fa'
+import { IoLogoWhatsapp } from 'react-icons/io'
+import styled from 'styled-components'
+import './WholesaleClothing.css'
+
+const StyledBiPhoneCall = styled(BiPhoneCall)`
+	transition: color 0.3s, background 0.3s;
+	&:hover {
+		color: #ff5555;  
+		background: white;
+	}
+`
+
+const StyledFaTelegramPlane = styled(FaTelegramPlane)`
+	transition: color 0.3s, background 0.3s;
+	&:hover {
+		color: #30a0de;
+		background: white;
+	}
+`
+
+const StyledIoLogoWhatsapp = styled(IoLogoWhatsapp)`
+	transition: color 0.3s, background 0.3s;
+	&:hover {
+		color: #13ca36;
+		background: white;
+	}
+`
+
 export default function WholesaleClothing() {
-	return <div></div>
+	const [hoverCall, setHoverCall] = useState<boolean>(false)
+	const [hoverTel, setHoverTel] = useState<boolean>(false)
+	const [hoverWhat, setHoverWhat] = useState<boolean>(false)
+
+	return (
+		<div>
+			<section id='wholesaleClothing'>
+				<div className='container'>
+					<div className='wholesaleClothing'>
+						<div className='wholesaaleClonthingTextInImages'>
+							<div className='wholesaleClothingText'>
+								<h4>
+									Оптом <span>одежда </span> из Кыргызстана
+								</h4>
+								<p>
+									От производителя, без посредников. <br />
+									Найдите лучшие товары вместе с нами!
+								</p>
+								<div className='whowholesaleClothingBlock'>
+									<div className='whowholesaleClothingBlockTexts'>
+										<h4>Выгодно </h4>
+										<h4 style={{ margin: '20px 0' }}>Надёжно </h4>
+										<h4>Удобно</h4>
+									</div>
+									<button>Каталог</button>
+								</div>
+							</div>
+							<div className='wholesaleClothingImages'>
+								<div
+									onMouseEnter={() => setHoverCall(true)}
+									onMouseLeave={() => setHoverCall(false)}
+								>
+									{hoverCall && <h4>Позвонить</h4>}
+									<StyledBiPhoneCall />
+								</div>
+								<div
+									onMouseEnter={() => setHoverTel(true)}
+									onMouseLeave={() => setHoverTel(false)}
+								>
+									{hoverTel && <h4>Telegram</h4>}
+									<StyledFaTelegramPlane />
+								</div>
+								<div
+									onMouseEnter={() => setHoverWhat(true)}
+									onMouseLeave={() => setHoverWhat(false)}
+								>
+									{hoverWhat && <h4>WhatsApp</h4>}
+									<StyledIoLogoWhatsapp />
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	)
 }
